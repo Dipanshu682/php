2f2b922e895df0904cbcf478200c7fb80f30fc6d
Dipanshu Sengar <dipanshusengar682@gmail.com>
2023-04-12 10:02:46 +0530

    Update database.php

--- assignment-1/database.php
+++ assignment-1/database.php
@@ -1,12 +1,31 @@
 <?php
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'phpdatabase';
+function addUser($name, $email, $password) {
+    // Connect to database
+	$DATABASE_HOST = 'localhost';
+	$DATABASE_USER = 'root';
+	$DATABASE_PASS = '';
+	$DATABASE_NAME = 'phpdatabase';
 
-$conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_error()) {
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+	$conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+	if (mysqli_connect_error()) {
+		exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+	}
+
+    // Insert variables into SQL query
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+    if (mysqli_query($conn, $sql)) {
+        header("Location: successful.php");
+        exit;
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    // Close database connection
+    mysqli_close($conn);
 }
- 
 ?>
